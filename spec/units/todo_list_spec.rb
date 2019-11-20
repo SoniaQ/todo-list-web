5712c46dcb09420c72f67a27dd61be967e0149de
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-20 13:04:25 +0000

    Update into separate unit test; correct rspec matcher

--- spec/units/todo_list_spec.rb
+++ spec/units/todo_list_spec.rb
@@ -1,10 +1,14 @@
 require 'todolist'
 describe ToDoList do
 
+  it 'responds to add with one argument' do
+    todolist = ToDoList.new
+    expect(todolist).to respond_to(:add).with(1).argument # best to separate 'expect' lines into separate unit tests
+  end
+
  it 'add something into the todolist' do
    todolist = ToDoList.new
-   expect(todolist).to respond_to(:add).with(1).argument
-   expect(todolist.add(list_item)).to have_content "Buy stamps"
+   expect(todolist.add(list_item)).to eq "Buy stamps" # 'have_content' is a capybara matcher used in feature tests; this is a unit test
  end
 
  it 'read all the items in the todo list' do
