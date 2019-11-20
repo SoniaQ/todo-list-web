470683979ad36bc99c3684c65cd0441d51ff1960
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-20 11:54:24 +0000

    Create failing unit test for add method with one argument

--- spec/units/todo_list_spec.rb
+++ spec/units/todo_list_spec.rb
@@ -3,6 +3,7 @@ describe ToDoList do
 
  it 'add something into the todolist' do
    todolist = ToDoList.new
+   expect(todolist).to respond_to(:add).with(1).argument
  end
 
  it 'read all the items in the todo list' do
